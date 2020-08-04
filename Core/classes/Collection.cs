4f98f2c94deb1feee99c45dba8542d437ac9941a
Diego Venturin <diego.venturin@gmail.com>
2020-08-04 22:39:30 +0200

    Collection updated

--- Core/classes/Collection.cs
+++ Core/classes/Collection.cs
@@ -3,11 +3,19 @@ using System;
 namespace OmegaConfig.Core
 {
 
+    /// <summary>
+    /// Define a collection to which an article belongs to. 
+    /// </summary>
     public class Collection
     {
         private string _id;
         private string _name;
 
+        ///<summary>
+        /// Create a new collection.
+        ///</summary>
+        ///<param name="id">The identifier code of the collection.</param>
+        ///<param name="name">The visible name of the collection.</param>
         public Collection(string id, string name)
         {
             if (String.IsNullOrWhiteSpace(_id) || String.IsNullOrWhiteSpace(name))
@@ -19,6 +27,13 @@ namespace OmegaConfig.Core
                 this._id = id;
                 this._name = name;
             }
+        }
+
+        /// <value>Gets or sets the name of the collection.</value>
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
         }
 
     }
--- Core/classes/Property.cs
+++ Core/classes/Property.cs
@@ -4,9 +4,15 @@ using System.Collections;
 namespace OmegaConfig.Core
 {
 
+    /// <summary>
+    /// A property of an article, belonging to a schema.
+    /// </summary>
     public class Property
     {
-        // TODO
+        private string _name;
+        
+
+        
 
     }
     
