217de7e8b9ac34723cc13e18261ba4550540e41e
Diego Venturin <diego.venturin@gmail.com>
2020-09-15 16:49:29 +0200

    Completed basic TextProperty

--- Core/classes/TextProperty.cs
+++ Core/classes/TextProperty.cs
@@ -1,5 +1,7 @@
 using System;
-using System.Collections;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
 
 namespace OmegaConfig.Core
 {
@@ -10,8 +12,169 @@ namespace OmegaConfig.Core
     public class TextProperty : GenericProperty
     {
 
+        // private fields
+        private readonly string _name;
+        private string _defaultText;
+        private string _currentText;
+        private int _maxLenght;
+        private int _minLenght;
+        private bool _required;
+
+        /// <summary>
+        /// Create a new property of text type.
+        /// </summary>
+        /// <param name="name">The identifier name of the property.</param>
+        public TextProperty(string name)
+        {
+            this._name = name;
+            this._required = false;
+            this._defaultText = "";
+            this._currentText = "";
+        }
+
+        /// <value>Gets the name of the property.</value>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+        }
+
+        /// <value>Gets or sets the value of the required.</value>
+        public bool Required
+        {
+            set
+            {
+                _required = value;
+            }
+            get
+            {
+                return _required;
+            }
+
+        }
+
+        /// <value>
+        /// Gets or sets the minimum lenght of the string.
+        /// </value>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the value is higher than the lenght of the max lenght, the current text or the default text, or is less then zero.</exception>
+        public int MinLenght
+        {
+            get => _minLenght;
+            set
+            {
+                if (value > _currentText.Length || value > _defaultText.Length)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueIncompatibleWithSetValues", Thread.CurrentThread.CurrentCulture));
+                }
+                else if (value > _maxLenght)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueHigherThanUpperBound", Thread.CurrentThread.CurrentCulture));
+                }
+                else if (value < 0)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueOutOfBounds", Thread.CurrentThread.CurrentCulture));
+                }
+                else
+                {
+                    this._minLenght = value;
+                }
+            }
+
+        }
+
+        /// <value>
+        /// Gets or sets the maximum lenght of the string.
+        /// </value>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the value is lower than the lenght of the minimum lenght, the current text or the default text.</exception>
+        public int MaxLenght
+        {
+            get => _maxLenght;
+            set
+            {
+                if (value < _currentText.Length || value < _defaultText.Length)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueIncompatibleWithSetValues", Thread.CurrentThread.CurrentCulture));
+                }
+                else if (value < _minLenght)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueLowerThanLowerBound", Thread.CurrentThread.CurrentCulture));
+                }
+                else
+                {
+                    this._maxLenght = value;
+                }
+            }
+
+        }
+
+        /// <value>
+        /// Gets or sets the default text of the property.
+        /// </value>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the lenght of the text is outside the limits.</exception>
+        public string DefaultText
+        {
+            get => _defaultText;
+            set
+            {
+                if (value is null)
+                {
+                    value = "";
+                }
+                if (value.Length < _minLenght || value.Length > _maxLenght)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueOutOfBounds", Thread.CurrentThread.CurrentCulture));
+                }
+                else
+                {
+                    this._defaultText = value;
+                }
+            }
+        }
+
+        /// <value>
+        /// Gets or sets the current value of the property.
+        /// </value>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when the value is outside upper or lower bound.</exception>
+        public string CurrentText
+        {
+            get => _currentText;
+            set
+            {
+                if (value is null)
+                {
+                    value = "";
+                }
+                if (value.Length < _minLenght || value.Length > _maxLenght)
+                {
+                    var resourceManager = new ResourceManager("Resources.ExceptionMessages", Assembly.GetExecutingAssembly());
+                    throw new ArgumentOutOfRangeException(value.ToString(Thread.CurrentThread.CurrentCulture), resourceManager.GetString("ValueOutOfBounds", Thread.CurrentThread.CurrentCulture));
+                }
+                else
+                {
+                    this._currentText = value;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Reset the current value to the default value.
+        /// </summary>
+        public void ResetToDefault()
+        {
+            this._currentText = _defaultText;
+        }
 
 
     }
-    
+
 }
